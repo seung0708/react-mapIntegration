286e72872174f774473cc5bb0e7c94c96638cf6f
Seung Hyun Kim <seung.kim0708@gmail.com>
2024-09-26 11:50:53 -0700

    working on adding a marker

--- src/index.js
+++ src/index.js
@@ -5,21 +5,58 @@ import {createRoot} from 'react-dom/client';
 const apiKey = process.env.MAP_API_KEY;
 
 const App = () => {
+    const [map, setMap] = useState(null)
+    const [marker, setMarker] = useState();
+    const [coordinates, setCoordinates] = useState({
+        lng: -118.243683,
+        lat: 34.0549
+    })
     useEffect(() => {
-        console.log(apiKey)
         mapboxgl.accessToken = apiKey
-        const map = new mapboxgl.Map({
+        
+        const newMap = new mapboxgl.Map({
             container: 'map',
             style: 'mapbox://styles/mapbox/dark-v10',
-            center: [-118.243683,34.0549],
+            center: [coordinates.lng, coordinates.lat],
             zoom: 10
         })
+
+        setMap(newMap)
+
+       // Add marker at the initial position
+       const mapMarker = new mapboxgl.Marker()
+       .setLngLat([coordinates.lng, coordinates.lat])
+       .addTo(map);
+
+        // Store marker in state
+        setMarker(mapMarker);
+        
+        return () => map.remove();
     
     },[])
 
+    const handleChange = (e) => {
+        const {name, value} = e.target; 
+        const parsedValue = parseFloat(value);
+        setCoordinates(prevState => ({
+            ...prevState,
+            [name]: isNaN(parsedValue) ? prevState[name] : parsedValue
+        }));
+
+        if(marker && !isNaN(parsedValue)) { 
+            marker.setLngLat([coordinates.lng, coordinates.lat])
+        }
+    }
+
     return (
     <>
-        <div id='map'></div>
+        <div>
+            <label>Longitude: </label>
+            <input type='number' name='lng' value={coordinates.lng} onChange={handleChange} />
+            <label>Latitude: </label>
+            <input type='number' name='lat' value={coordinates.lat} onChange={handleChange} />
+        </div>
+        <div id='map' style={{ width: '100%', height: '500px' }}></div>
     </>
     )
 }
